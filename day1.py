bfff04f6998c9559c9cdd01ee90eaea5621dec52
monoguitari <jesus_rodriguez@brown.edu>
2025-12-05 12:51:13 -0500

    feat: day 1 part 1

--- day1.py
+++ day1.py
@@ -1,1 +1,19 @@
 print("Hello Advent of Code")
+input = []
+with open("inputs/day1.txt") as f:
+    for line in f:
+        input.append(line.strip()) # removes newline char at end
+
+ans = 0
+cur_pos = 50
+for rotation in input:
+    dir = rotation[0]
+    dist = int(rotation[1:])
+    if dir == "L":
+        cur_pos = (cur_pos - dist) % 100
+    else:
+        cur_pos = (cur_pos + dist) % 100
+    if cur_pos == 0:
+        ans += 1
+
+print(ans)
